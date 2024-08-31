9abf701ea2869c76084a85b214596740a188f67d
LordEricDraven <58068655+LordEricDraven@users.noreply.github.com>
2024-08-31 13:59:33 -0400

    update assignment 14

--- assignment14/src/main/java/com/coderscampus/assignment14/Assignment14Application.java
+++ assignment14/src/main/java/com/coderscampus/assignment14/Assignment14Application.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Assignment14Application {
 
-	// github test
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Assignment14Application.class, args);
